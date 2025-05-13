c385e5c61dba08edaea39c738db5057b8b2fe89d
Zhou Linquan <awkwarp@outlook.com>
2025-05-14 05:42:52 +0800

    ver1.0.6.4 rotated gray image

--- jpg2gray.py
+++ jpg2gray.py
@@ -1,6 +1,12 @@
 from PIL import Image
 
-# 使用 Pillow 转为灰阶
+# 使用 Pillow 转为灰阶 + 旋转
 def run():
-    img = Image.open("RGBoutput.jpg").convert("L")  # "L" 表示灰度模式
-    img.save("Grayoutput.jpg", "JPEG")
+    # 打开并转换为灰度图像
+    img = Image.open("RGBoutput.jpg").convert("L")  # "L" = 灰阶
+
+    # 顺时针旋转 90 度（rotate 默认是逆时针，所以用 -90）
+    rotated = img.rotate(-90, expand=True)
+
+    # 保存输出
+    rotated.save("Grayoutput.jpg", "JPEG")
